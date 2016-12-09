80923fd537eb7381006e30756399b11b34bd70f5
Michael V. DePalatis <mike@depalatis.net>
2016-12-09 14:06:40 -0500

    Also include Isaac's zmqsocket changes

--- source/pylib/zmqsocket.py
+++ source/pylib/zmqsocket.py
@@ -1,6 +1,7 @@
 import logging
 from threading import Thread
 import zmq
+import json
 from zmq.eventloop.ioloop import ZMQIOLoop
 from zmq.eventloop.zmqstream import ZMQStream
 from messages import RAMMessage
@@ -17,12 +18,13 @@ class SocketServer(Thread):
     :param str address: Address and port to bind to.
 
     """
-    def __init__(self, ctx=None, loop=None, address="tcp://192.168.137.200:8889"):
+    def __init__(self, ctx=None, loop=None, address="tcp://*:8889"):
         self.ctx = ctx or zmq.Context()
         self._loop = loop or ZMQIOLoop()
+        self._recv_callback = self.default_callback
 
         self.address = address
-        self.connected = False
+        self._connected = False
 
         self.sock = self.ctx.socket(zmq.PAIR)
         # self.sock.setsockopt(zmq.LINGER, 0)  # discard messages that can't be sent
@@ -31,6 +33,16 @@ class SocketServer(Thread):
         self.sock.bind(self.address)
 
         super(SocketServer, self).__init__()
+
+    def register_recv_callback(self, callback):
+        self._recv_callback = callback
+
+    def default_callback(self, msg):
+        logger.error("Message handling uninitialized! Msg {} received!".format(msg))
+
+    @property
+    def connected(self):
+        return self._connected
 
     def on_recv(self, multipart):
         """Callback to handle incoming messages."""
@@ -49,8 +61,8 @@ class SocketServer(Thread):
 
             if msg_type == "SYNC":
                 self.send(SyncMessage(msg["num"]))
-            else:
-                logger.info("doing nothing on %s message" % msg_type)
+            self._recv_callback(msg)
+            logger.info("executing callback for %s message" % msg_type)
 
     def send(self, msg):
         """Transmit a message to the host PC.
@@ -63,20 +75,26 @@ class SocketServer(Thread):
         try:
             self.stream.send(out, flags=zmq.NOBLOCK)
         except Exception as e:
-            print(e)
+            logger.error("Sending failed: {}", e)
+
+    def wait_for_connection(self):
+        logger.debug("Waiting to receive initialization json")
+        logger.debug(self.sock.recv_json())  # Blocking
+        logger.debug("Initialization json received")
+        self._connected = True
 
     def stop(self):
         """Stop the event loop."""
         logger.info("Shutting down ZMQ event loop...")
         self._loop.stop()
+        self._connected = False
 
     def run(self):
+        """Starts event loop. Blocks"""
         self._loop.start()
 
 
 if __name__ == "__main__":
-    import time
-    from threading import Timer
     from messages import *
 
     logging.basicConfig(level=logging.DEBUG)
