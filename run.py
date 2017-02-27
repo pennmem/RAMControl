4fc0602d917bbb265cc498355d2e197e6a2ca805
Michael V. DePalatis <mike@depalatis.net>
2017-02-27 14:55:52 -0500

    Fix build_exp_config, add --no-host option
    How did this ever even work before???

--- run.py
+++ run.py
@@ -43,16 +43,28 @@ def run_experiment(exp_config):
     env = os.environ.copy()
     env["PYTHONPATH"] = absjoin(".")
 
+    # Flag that we shouldn't try to connect to the host PC (for development
+    # purposes)
+    env["RAM_CONFIG"] = json.dumps(dict(no_host=exp_config.pop("no_host")))
+
     p = subprocess.Popen(args, env=env, cwd=exp_dir)
     p.wait()
 
 
 def build_exp_config(json_config, experiment, **kwargs):
+    """Build experiment configuration from the JSON config file.
+
+    :param dict json_config:
+    :param str experiment: Name of the experiment.
+    :param dict kwargs:
+    :rtype: dict
+
+    """
     exp_config = json_config['experiments'][experiment]
     json_config.update(exp_config)
     json_config.update(kwargs)
     json_config['experiment'] = experiment
-    return config
+    return json_config
 
 
 def parse_args():
@@ -62,6 +74,8 @@ def parse_args():
     parser.add_argument('--resolution', '-r', dest='resolution', default=None, help='Screen resolution')
     parser.add_argument('--archive', '-a', dest='archive', default=None, help='Data storage directory')
     parser.add_argument('--no-fs', dest='no_fs', default=False, action='store_true', help='Turn off fullscreen')
+    parser.add_argument("--no-host", default=False, action="store_true",
+                        help="Run without connecting to the host PC (for development only)")
     parser.add_argument("--experiment-dir", "-e", help="Directory containing experiments")
     return {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
 
