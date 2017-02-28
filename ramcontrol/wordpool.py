b978e41bc7c41d9b2dd0fc9426d0ce560e9b199b
Michael V. DePalatis <mike@depalatis.net>
2017-02-28 16:56:51 -0500

    Begin implementing wordpool generation
    This will be moved to its own separate module for portability and
    reuse.

--- ramcontrol/wordpool.py
+++ ramcontrol/wordpool.py
@@ -3,7 +3,6 @@
 import random
 import codecs
 import unicodedata
-import shutil
 import six
 
 
@@ -17,7 +16,7 @@ class WordList(list):
     def __init__(self, iterable=None):
         if isinstance(iterable, (six.string_types, six.text_type)):
             with codecs.open(iterable, encoding="utf-8") as f:
-                super(WordList, self).__init__(f.read().split())
+                super(WordList, self).__init__(sorted(f.read().split()))
         else:
             super(WordList, self).__init__(iterable)
 
@@ -34,10 +33,25 @@ class WordPool(object):
     """Handles operations for entire word pools.
 
     :param str path: Path to word pool.
+    :param int num_lists: The number of lists to generate out of the entire
+        pool.
 
     """
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path, num_lists=25):
+        words = WordList(path).shuffle()
+        assert len(words) % num_lists == 0
+        step = len(words) / num_lists
+        self.lists = [WordList(words[step*n:(step*n + step)]) for n in range(num_lists)]
+        assert all([len(l) == step for l in self.lists])
+
+    def __str__(self):
+        return str(self.lists)
+
+    def __len__(self):
+        return len(self.lists)
+
+    def __getitem__(self, item):
+        return self.lists[item]
 
     def save(self, dest):
         """Copy the word pool with and without accents to a new location.
@@ -98,3 +112,6 @@ def remove_accents(input_str):
     nkfd_form = unicodedata.normalize('NFKD', input_str)
     return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
+
+if __name__ == "__main__":
+    print(WordPool("/Users/depalati/src/RAMControl/tests/data/wordpool_en.txt"))
--- tests/test_wordpool.py
+++ tests/test_wordpool.py
@@ -2,32 +2,18 @@
 
 import os.path as osp
 from copy import deepcopy
+from collections import Counter
+import itertools
 import six
 import pytest
 from ramcontrol.util import data_path
 from ramcontrol import wordpool
-from ramcontrol.wordpool import WordList
+from ramcontrol.wordpool import WordList, WordPool
 
 
-def test_shuffle_inner_lists():
-    lists = [list(range(10)) for _ in range(10)]
-    other = deepcopy(lists)
-    wordpool.shuffle_inner_lists(other)
-    assert lists != other
-
-
-@pytest.mark.skip(reason="Not sure what this means yet")
-def test_shuffle_together():
-    pass
-
-
-def test_read_list():
-    filename = osp.join(data_path(), "lures_EN.txt")
-    lures = wordpool.read_list(filename)
-    assert isinstance(lures, list)
-    assert len(lures) is 65
-    for lure in lures:
-        assert isinstance(lure, six.text_type)
+@pytest.fixture
+def wordpool_en():
+    yield osp.join(data_path(), "wordpool_en.txt")
 
 
 @pytest.mark.skip(reason="Not sure how to test this right now")
@@ -55,5 +41,49 @@ class TestWordList:
 
 
 class TestWordPool:
+    def test_create(self, wordpool_en):
+        pool = WordPool(wordpool_en, 25)
+        with open(wordpool_en) as f:
+            words = f.read().split()
+
+        # Check types
+        for l in pool.lists:
+            assert isinstance(l, WordList)
+
+        # Check shape
+        assert len(pool.lists) is 25
+        assert all([len(l) == 12 for l in pool.lists])
+
+        # Check all words are present
+        for word in words:
+            assert any([word in lst for lst in pool.lists])
+
+        # Check uniqueness
+        counter = Counter(list(itertools.chain(*pool.lists)))
+        assert len(list(counter.elements())) == len(words)
+
+        # Check num_lists arg fails if wrong
+        with pytest.raises(AssertionError):
+            WordPool(wordpool_en, 26)
+            WordPool(wordpool_en, 50)
+
+        # Check shuffling performed
+        pool2 = WordPool(wordpool_en, 25)
+        for n in range(len(pool.lists)):
+            assert pool.lists[n] != pool2.lists[n]
+
+    def test_str(self, wordpool_en):
+        pool = WordPool(wordpool_en)
+        assert str(pool) == str(pool.lists)
+
+    def test_getitem(self, wordpool_en):
+        pool = WordPool(wordpool_en)
+        for n in range(len(pool.lists)):
+            assert pool[n] == pool.lists[n]
+
+    def test_len(self, wordpool_en):
+        pool = WordPool(wordpool_en)
+        assert len(pool) == len(pool.lists)
+
     def test_save(self):
-        assert pytest.raises(NotImplementedError)
+        pass
