db06a2d88aa728fd8cd21eee4bb1e3999d88169c
Michael V. DePalatis <mike@depalatis.net>
2017-03-06 10:36:39 -0500

    Test assign_list_types

--- tests/test_listgen.py
+++ tests/test_listgen.py
@@ -13,9 +13,32 @@ def test_generate_session_pool():
         for list_ in session:
             assert type(list_) is WordList
             assert len(list_) is 12
-        assert session[0].metadata["type"] == "PRACTICE"
 
     with pytest.raises(AssertionError):
         listgen.generate_session_pool(13)
         listgen.generate_session_pool(num_lists=random.randrange(26))
         listgen.generate_session_pool(language="DA")
+
+
+def test_assign_list_types():
+    session = listgen.generate_session_pool()
+    session = listgen.assign_list_types(session, 3, 7, 11, 4)
+    n_ps, n_ns, n_s = 0, 0, 0
+    for n, list_ in enumerate(session):
+        if n == 0:
+            assert session[n].metadata["type"] == "PRACTICE"
+            continue
+        kind = session[n].metadata["type"]
+        if kind == "PS ENCODING":
+            n_ps += 1
+        elif kind == "NON-STIM ENCODING":
+            n_ns += 1
+        elif kind == "STIM ENCODING":
+            n_s += 1
+
+    for n in range(1, 4):
+        assert session[n].metadata["type"] == "NON-STIM ENCODING"
+
+    assert n_ps is 4
+    assert n_ns is 10
+    assert n_s is 11
