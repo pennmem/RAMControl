d7cfe290cb3d6044eb0e5f41a3ba7e596b574117
Michael V. DePalatis <mike@depalatis.net>
2017-03-12 14:50:59 -0400

    Include convience method to show text and wait for keypress

--- ramcontrol/epl.py
+++ ramcontrol/epl.py
@@ -143,3 +143,18 @@ class PyEPLHelpers(object):
             self.play_movie_sync(filename, stop_bc)
             seen_once = True
             self.video.unshow(shown)
+
+    def show_text_and_wait_for_keyboard_input(self, text, keys=["SPACE"]):
+        """Display text and wait for the user to hit a key.
+        
+        :param str text: Text to display.
+        :param list keys: List of keys to accept.
+        :returns: Tuple of key pressed, timestamp of key press.
+
+        """
+        if len(keys) > 1:
+            raise NotImplementedError("TODO: allow more than just SPACE key")
+        bc = ButtonChooser(Key('SPACE'))
+        self.video.clear('black')
+        _, key, timestamp = Text(text).present(self.clock, bc=bc)
+        return key, timestamp
