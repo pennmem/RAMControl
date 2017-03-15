ce8ce54f25f2e61a07a82a72f93560d20f962c25
Michael V. DePalatis <mike@depalatis.net>
2017-03-14 20:56:35 -0400

    Genericize log server to take handlers

--- ramcontrol/log.py
+++ ramcontrol/log.py
@@ -8,30 +8,44 @@ from multiprocessing import Process
 DEFAULT_LOG_PORT = 9123
 
 
-def log_server(filename, host="127.0.0.1", port=DEFAULT_LOG_PORT):
+def log_server(handlers=[], host="127.0.0.1", port=DEFAULT_LOG_PORT,
+               level=logging.INFO):
     """Target for a process to run a server to aggregate and record all log
     messages to disk.
 
-    :param str filename: Log file name.
+    :param list handlers: List of log handlers to use. If not given, only a
+        :class:`logging.NullHandler` will be used.
     :param str host: Host to bind to.
     :param int port: UDP port number to bind to.
+    :param int level: Minimum log level.
 
     """
     queue = Queue()
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
+    if len(handlers) == 0:
+        handlers.append(logging.NullHandler())
+
+    for handler in handlers:
+        handler.setLevel(level)
+        root_logger.addHandler(handler)
 
     def consume():
         while True:
             record = queue.get()
-            print({
-                "timestamp": record.created,
-                "name": record.name,
-                "levelname": record.levelname,
-                "pathname": record.pathname,
-                "lineno": record.lineno,
-                "threadName": record.threadName,
-                "processName": record.processName,
-                "msg": record.msg
-            })
+            for handler in handlers:
+                logging.getLogger(record.name).handle(record)
+            # print({
+            #     "timestamp": record.created,
+            #     "name": record.name,
+            #     "levelname": record.levelname,
+            #     "pathname": record.pathname,
+            #     "lineno": record.lineno,
+            #     "threadName": record.threadName,
+            #     "processName": record.processName,
+            #     "msg": record.msg
+            # })
 
     class Handler(socketserver.DatagramRequestHandler):
         def handle(self):
@@ -52,7 +66,8 @@ def log_server(filename, host="127.0.0.1", port=DEFAULT_LOG_PORT):
     server.serve_forever()
 
 
-def create_logger(name, host="127.0.0.1", port=DEFAULT_LOG_PORT):
+def create_logger(name, host="127.0.0.1", port=DEFAULT_LOG_PORT,
+                  level=logging.INFO):
     """Create a logger and setup appropriately. For loggers running outside of
     the main process, this must be called after the process has been started
     (i.e., in the :func:`run` method of a :class:`multiprocessing.Process`
@@ -61,10 +76,11 @@ def create_logger(name, host="127.0.0.1", port=DEFAULT_LOG_PORT):
     :param str name: Name of the logger.
     :param str host: Host address.
     :param int port: UDP port.
+    :param int level: Minimum log level.
 
     """
     logger = logging.getLogger(name)
-    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(level)
     logger.addHandler(DatagramHandler(host, port))
     return logger
 
@@ -110,7 +126,14 @@ if __name__ == "__main__":
             logger.debug("hi %d", n)
             time.sleep(random.randint(1, 3))
 
-    server = Process(target=log_server, args=("logs.log",))
+    handlers = [
+        RotatingFileHandler("/tmp/logs.log", maxBytes=10e6, backupCount=5)
+    ]
+
+    for handler in handlers:
+        handler.setFormatter(logging.Formatter("[%(levelname)1.1s %(asctime)s] %(pathname)s:%(lineno)d%(message)s"))
+
+    server = Process(target=log_server, args=(handlers,))
     server.start()
 
     procs = [Process(target=log_producer) for _ in range(1)]
