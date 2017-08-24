423bf8e56833625954b0b25025754ba9888f6381
Michael V. DePalatis <mike@depalatis.net>
2017-08-24 10:22:37 -0400

    Add config files for FR6, catFR6

---
+++ ramcontrol/configs/FR/FR6_config.py
@@ -0,0 +1,17 @@
+experiment = "FR6"
+version = "6.0.0"
+
+# Number of baseline lists (excluding practice)
+n_baseline = 3
+
+# Number of stim lists
+n_stim = 16  # total
+n_stim_A = 5
+n_stim_B = 5
+n_stim_AB = 6
+
+# Number of non-stim lists
+n_nonstim = 6
+
+# Enable VAD
+vad_during_retrieval = True
